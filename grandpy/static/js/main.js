639af2f62b58d3131e268d96ab0eeaa313c09f38
HVincent <vhouillon@gmail.com>
2019-02-23 18:47:13 +0100

    add Google Maps with JS on Webpage & add route get_json

---
+++ grandpy/static/js/main.js
@@ -0,0 +1,47 @@
+/* Make an AJAX GET call
+Takes into parameters the target URL and callback function called on success */
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Calls the callback function by passing the response of the request
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Network error with URL " + url);
+    });
+    req.send(null);
+};
+
+
+ajaxGet('/get_json', function (response) {
+    let coordinates = JSON.parse(response);
+    var lat = coordinates['latitude'];
+    var lng = coordinates['longitude'];
+    myMap(lat, lng)
+});
+
+
+function myMap(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    /* Object containing properties with predefined identifiers in Google Maps 
+    to define options for displaying our map */
+
+    var options = {
+        center: latlng,
+        zoom: 19,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("googlemaps"), options);
+
+    //Marker creation
+    var marqueur = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map
+
+    });
+};
--- grandpy/views.py
+++ grandpy/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,11 +6,17 @@ app = Flask(__name__)
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
-
 @app.route('/')
 def index():
-    gm = "Google Maps here"
-    return render_template('index.html', GoogleMaps=gm)
+    return render_template('index.html')
+
+@app.route('/get_json')
+def get_json():
+    """Return data in JSON Format."""
+    return jsonify(
+        latitude=48.874779,
+        longitude=2.350489
+    )
 
 
 if __name__ == '__main__':
