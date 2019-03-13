311afbdbf53967dcef92de679e906774ded8aa68
Vincent Houillon <vhouillon@gmail.com>
2019-03-13 16:11:47 +0100

    fix typo

--- grandpy/__init__.py
+++ grandpy/__init__.py
@@ -1,11 +1,5 @@
+from .views import app
 from flask import Flask
-import os
+
 
 app = Flask(__name__)
-if app.config['ENV'] != 'development':
-    app.config.from_object('config.DevelopmentConfig')
-else:
-    app.config['GMAPS_KEY'] = os.environ.get('GMAPS_KEY')
-
-
-from .views import app
